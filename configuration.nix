fa44d836bfa21df7c79ca4623ab720c52a773e36
Mateo <79337611+mateowoetam@users.noreply.github.com>
2024-07-22 14:49:01 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,180 +1,29 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
+{ config, pkgs, ...}:
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+    ./doas.nix
+    ./boot-loader.nix
+    ./networking.nix
+    ./display-manager.nix
+    ./time-locale.nix
+    ./sound.nix
+    ./flakes.nix
+    ./users.nix
+    ./printing.nix
+    ./packages.nix
+    ./flatpak.nix
+    ./steam-hardware.nix
+  ];
 
-  # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/vda";
-  boot.loader.grub.useOSProber = true;
-
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Mullvad DNS
-  #networking.nameservers = [ "194.242.2.2#dns.mullvad.net" "194.242.2.3#adblock.dns.mullvad.net" ];
-  #
-  #services.resolved = {
-  #  enable = true;
-  #  dnssec = "true";
-  #  domains = [ "~." ];
-  #  fallbackDns = [ "194.242.2.3#adblock.dns.mullvad.net" ];
-  #  dnsovertls = "true";
-  #};
-
-  # Set your time zone.
-  time.timeZone = "America/Mexico_City";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
-  # Enable Doas
-  security.doas.enable = true;
-  security.sudo.enable = false;
-  # Configure DOAS
-  security.doas.extraRules = [{
-    users = ["user"];
-    # Optional, retains environment variables while running commands
-    # e.g. retains your NIX_PATH when applying your config
-    keepEnv = true;
-    persist = true;  # Optional, only require password verification a single time
-  }];
-
-
- # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.user = {
-    isNormalUser = true;
-    description = "user";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      kdePackages.kate
-    #  thunderbird
-    ];
-  };
-
-  # Steam Hardware
-  hardware.steam-hardware.enable = true;
-
-  # Configure firefox.
-  programs.firefox.enable = false;
-
-  # Configure Fish Shell
-  programs.fish.enable = true;
-
+  # Set the state version for NixOS
+  system.stateVersion = "24.05"; # Adjust this to math your NixOS version
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # Configure Flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-
-  # Librewolf
-  pkgs.librewolf
-
-  # Fastfetch
-  pkgs.fastfetch
-
-  # Fish SHell
-  pkgs.fish
-  fishPlugins.done
-  fishPlugins.fzf-fish
-  fishPlugins.forgit
-  fishPlugins.hydro
-  fzf
-  fishPlugins.grc
-  grc
-
-  # Git
-  pkgs.git
-
-  # Vesktop/Discord
-  pkgs.vesktop
-
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
-
+  # Auto system update
+  system.autoUpgrade = {
+        enable = true;
+  };
 }
-
