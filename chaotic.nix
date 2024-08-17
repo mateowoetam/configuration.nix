d99bb41514207a21387e271f1763189133e978a3
Mateo <79337611+mateowoetam@users.noreply.github.com>
2024-08-17 14:33:31 +0000

    added chaotic conifg

---
+++ chaotic.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ...}:
+{
+    environment.systemPackages = [
+    pkgs.mangohud32_git
+    pkgs.mangohud_git
+    pkgs.mesa32_git
+    pkgs.mesa32_git
+    pkgs.gamescope_git
+    ];
+
+    boot.kernelPackages = pkgs.linuxPackages_cachyos;
+    #chaotic.scx.enable = true;
+
+    #chaotic.hdr.enable = true;
+}
+
