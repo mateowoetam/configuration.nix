008bef3d2a678c826b201dcf04c577aaef76d31b
Mateo <79337611+mateowoetam@users.noreply.github.com>
2025-03-16 14:04:51 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,16 +3,21 @@
   description = "My first Flake!";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.4.1";
     stylix.url = "github:danth/stylix";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nix-gaming.url = "github:fufexan/nix-gaming";
+    #suyu = {
+     # url = "git+https://git.suyu.dev/suyu/nix-flake";
+     # inputs.nixpkgs.follows = "nixpkgs";
+    #};
+    #nix-software-center.url = "github:snowfallorg/nix-software-center";
   };
 
   outputs = {self, nixpkgs, ...}@inputs: {
