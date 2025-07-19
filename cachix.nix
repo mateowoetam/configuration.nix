5ada2eddc24edb48b7f27b3807ed47000c07a4ac
Mateo <79337611+mateowoetam@users.noreply.github.com>
2025-07-19 02:14:52 +0000

    Add files via upload

---
+++ cachix.nix
@@ -0,0 +1,17 @@
+# WARN: this file will get overwritten by $ cachix use <name>
+{ pkgs, lib, ... }:
+
+let
+  folder = ./cachix;
+  toImport = name: value: folder + ("/" + name);
+  filterCaches = key: value: value == "regular" && lib.hasSuffix ".nix" key;
+  imports = lib.mapAttrsToList toImport (lib.filterAttrs filterCaches (builtins.readDir folder));
+in
+{
+  inherit imports;
+  nix.settings.substituters = [ 
+  	"https://nix-community.cachix.org/"
+  	"https://chaotic-nyx.cachix.org/"
+  	"https://cache.nixos.org/" 
+  ];
+}
