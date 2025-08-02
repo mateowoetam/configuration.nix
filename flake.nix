9432380de7e57b0669df77206c9c319901630d60
Mateo <79337611+mateowoetam@users.noreply.github.com>
2025-08-02 17:52:05 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,40 +2,57 @@
   description = "My NixOS Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";  #nixos-25.05
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # nixos-25.05
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager"; #release-25.05
+      url = "github:nix-community/home-manager"; # release-25.05
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-flatpak.url = "github:gmodena/nix-flatpak";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nix-gaming.url = "github:fufexan/nix-gaming";
     lanzaboote = {
       url = "github:nix-community/lanzaboote";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    cachix.url = "github:cachix/cachix";
   };
 
-  outputs = { self, nixpkgs, chaotic, home-manager, nixos-hardware, nix-gaming, lanzaboote, ... }@inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./configuration.nix
-        nixos-hardware.nixosModules.asus-zephyrus-ga402x-amdgpu
-        chaotic.nixosModules.default
-        nix-gaming.nixosModules.platformOptimizations
-        lanzaboote.nixosModules.lanzaboote
-      ];
-    };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      chaotic,
+      home-manager,
+      nix-flatpak,
+      nixos-hardware,
+      nix-gaming,
+      lanzaboote,
+      cachix,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+          chaotic.nixosModules.default
+          nix-flatpak.nixosModules.nix-flatpak
+          nixos-hardware.nixosModules.asus-zephyrus-ga402x-amdgpu
+          nix-gaming.nixosModules.platformOptimizations
+          lanzaboote.nixosModules.lanzaboote
+        ];
+      };
 
-    homeConfigurations.user = home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      extraSpecialArgs = { inherit inputs; };
-      modules = [
-        ./home.nix
-        chaotic.homeManagerModules.default
-      ];
+      homeConfigurations.user = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        extraSpecialArgs = { inherit inputs; };
+        modules = [
+          ./home.nix
+          chaotic.homeManagerModules.default
+          nix-flatpak.homeManagerModules.nix-flatpak
+        ];
+      };
     };
-  };
 }
